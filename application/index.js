89e5d69f078814371dc7672373e29d2a3a4bc09e
Eduardo Dornelles Souza <eduardodornellessouza@gmail.com>
2022-04-25 20:42:16 -0300

    commit

--- application/index.js
+++ application/index.js
@@ -1,18 +1,30 @@
+// import express framework
 const express = require("express");
+// import module 
 const os = require("os");
+// set express
 const app = express();
+// set default port
 const port = 80;
+// set date/time
 const datahora = new Date();
+// set engine
 const nodeversion = process.versions.v8;
+// set hostname by os module
 const hostname = os.hostname();
+// import security package
 const http = require("http");
+// set ip by visitor from ip package
 const ip = require('ip');
 
+// main method
 app.get("/challenge",(req, res) => {
+    console.log(ip.address());
     res.json(returndate);
     }
 );
 
+// response from get no main method
 let returndate = [
     {
         timestamp: `${datahora}`,
@@ -22,4 +34,5 @@ let returndate = [
     }
 ]
 
+// app listem and console log
 app.listen(port, () => console.log(`Server running on port:${port}`));

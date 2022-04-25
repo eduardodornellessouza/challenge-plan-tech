36a2269a8ac0e32aea76e3bb121843c1c88dfb5d
Eduardo Dornelles Souza <eduardodornellessouza@gmail.com>
2022-04-25 10:54:48 -0300

    commit

--- application/index.js
+++ application/index.js
@@ -6,28 +6,20 @@ const datahora = new Date();
 const nodeversion = process.versions.v8;
 const hostname = os.hostname();
 const http = require("http");
-
-function pegaporradoip () {
-    http.get({'host': 'api.ipify.org', 'port': 80, 'path': '/'}, function(resp) {
-        resp.on('data', function(ip) {
-            console.log(ip);
-        });
-      });
-
-}
+const ip = require('ip');
 
 app.get("/challenge",(req, res) => {
-    pegaporradoip();  
-    let returndate = [
-        {
-            timestamp: `${datahora}`,
-            hostname: `${hostname}`,
-            engine: `${nodeversion}`,
-            visitorip: pegaporradoip()
-        }
-    ]
     res.json(returndate);
     }
 );
+
+let returndate = [
+    {
+        timestamp: `${datahora}`,
+        hostname: `${hostname}`,
+        engine: `${nodeversion}`,
+        visitorip: `${ip.address()}`
+    }
+]
 
 app.listen(port, () => console.log(`Server running on port:${port}`));
